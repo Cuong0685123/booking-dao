447b5e7fa064e5f5acc85c28303b691f358b2aac
Cuong0685123 <eticon0685@gmail.com>
2024-09-24 06:41:14 +0700

    update HotelRepo

--- src/main/java/com/tc/booking/repo/HotelRepository.java
+++ src/main/java/com/tc/booking/repo/HotelRepository.java
@@ -1,9 +1,14 @@
 package com.tc.booking.repo;
 
 import com.tc.booking.model.entity.Hotel;
+import com.tc.booking.model.entity.Room;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface HotelRepository extends JpaRepository<Hotel, Integer> {
+   List<Room> findByHotelId(int hotelId);
+   List<Hotel> findAll();
+   
 }
